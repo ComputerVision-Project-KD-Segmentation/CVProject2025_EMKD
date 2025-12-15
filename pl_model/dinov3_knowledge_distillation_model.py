0b8ae32b2bfba77c649a75704878cd15db668d2f
seokhun <sostar0832@cau.ac.kr>
2025-12-15 18:13:09 +0900

    dinov3 중간 피쳐 수정

--- pl_model/dinov3_knowledge_distillation_model.py
+++ pl_model/dinov3_knowledge_distillation_model.py
@@ -80,10 +80,10 @@ class Dinov3KnowledgeDistillationPLModel(BasePLModel):
         loss_seg = calc_loss(output, mask)
 
         loss_pmd = prediction_map_distillation(output, t_out)
-        loss_imd = importance_maps_distillation(low, t_low)
-        loss_rad = region_affinity_distillation(low, t_low, mask)
-        # loss_imd = importance_maps_distillation(low, t_low) + importance_maps_distillation(high, t_high)
-        # loss_rad = region_affinity_distillation(low, t_low, mask) + region_affinity_distillation(high, t_high, mask)
+        # loss_imd = importance_maps_distillation(low, t_low)
+        # loss_rad = region_affinity_distillation(low, t_low, mask)
+        loss_imd = importance_maps_distillation(low, t_low) + importance_maps_distillation(high, t_high)
+        loss_rad = region_affinity_distillation(low, t_low, mask) + region_affinity_distillation(high, t_high, mask)
 
         loss_dict = self.dsd_loss(
             feat_student=output,
