e611f59b5bdf11a46b3384a73b242fb3e7696e17
seokhun <sostar0832@cau.ac.kr>
2025-12-15 14:23:19 +0900

    dinov3 중간 피쳐 수정

--- models/dinov3_vision_transformer.py
+++ models/dinov3_vision_transformer.py
@@ -1110,8 +1110,8 @@ class Primus_Multiscale(AbstractDynamicNetworkArchitectures):
         hier = self.dino_encoder.get_intermediate_layers(x,  n=self.interaction_indices, reshape = True)
         
         # print("Primus_Multiscale hier:", len(hier), hier[0].shape)
-        low = hier[0]
-        high = torch.cat(hier[:1], dim=1)
+        low = torch.cat(hier[:1], dim=1)
+        high = torch.cat(hier, dim=1)
 
         # low = torch.cat(hier[:1], dim=1)
         # high = torch.cat(hier, dim=1)
